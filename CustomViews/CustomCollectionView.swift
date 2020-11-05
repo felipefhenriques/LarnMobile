bb0231cfce6ff8c125750c1b1a238cc3ad63ed3c
Rogerio <Rogerios.lucon@gmail.com>
2020-11-05 10:04:11 -0300

    Arquivos puxados da outra versão.

---
+++ CustomViews/CustomCollectionView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionView.swift
+//  Larn
+//
+//  Created by Rogerio Lucon on 29/10/20.
+//
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    
+    
+}
---
+++ Model/Aula+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Aula+CoreDataProperties.swift
+//  Larn
+//
+//  Created by Rogerio Lucon on 30/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Aula {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Aula> {
+        return NSFetchRequest<Aula>(entityName: "Aula")
+    }
+    @NSManaged public var id: UUID?
+    @NSManaged public var data: Date?
+    @NSManaged public var tema: String?
+    @NSManaged public var valor: NSDecimalNumber?
+    @NSManaged public var requisitos: String?
+    @NSManaged public var conteudo: String?
+    @NSManaged public var descricao: String?
+    @NSManaged public var image: Data?
+
+}
+
+extension Aula : Identifiable {
+
+}
