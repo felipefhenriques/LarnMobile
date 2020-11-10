cf37e48b724171a2eb60fc47660a327de2e35635
Rogerio <Rogerios.lucon@gmail.com>
2020-11-10 13:40:00 -0300

    Entidade Materia criada. ViewController (Main Storyboard) cria categorias caso nao encontre.

--- Larn/ViewController.swift
+++ Larn/ViewController.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let contex = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var categorys: [Materia] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchData()
     }
+    
+    func fetchData(){
+        do {
+            categorys = try contex.fetch(Materia.fetchRequest())
+            generateCategorys()
+        } catch {
 
+        }
+    }
+    
+    func create(name: String){
+       
+       let materia = Materia(context: self.contex)
+           
+        materia.materia = name
+        
+        do {
+            try contex.save()
+        } catch {
+            fatalError("Materia \(materia.materia) nao pode ser adicionada")
+        }
+    }
+    
+    func generateCategorys(){
+        var dev = false
+        var business = false
+        var design = false
+        var finances = false
+        var marketing = false
+        var artes = false
+        
+        for category in categorys {
+            switch category.materia {
+            case "Desenvolvimento":
+                dev = true
+            case "Negocios":
+                business = true
+            case "Design":
+                design = true
+            case "Finanças":
+                finances = true
+            case "Marketing":
+                marketing = true
+            case "Artes":
+                artes = true
+            default:
+                break
+            }
+        }
+        
+        if !dev {
+            create(name: "Desenvolvimento")
+        }
+        if !business {
+            create(name: "Negocios")
+        }
+        if !design {
+            create(name: "Design")
+        }
+        if !finances {
+            create(name: "Finanças")
+        }
+        if !marketing {
+            create(name: "Marketing")
+        }
+        if !artes {
+            create(name: "Artes")
+        }
+    }
 
 }
 
---
+++ Model/Materia+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Materia+CoreDataClass.swift
+//  Larn
+//
+//  Created by Rogerio Lucon on 10/11/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Materia)
+public class Materia: NSManagedObject {
+
+}
